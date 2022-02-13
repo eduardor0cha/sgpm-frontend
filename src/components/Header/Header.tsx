1e2ca4be3d6c9af7edb7e0ce100db204ec117e81
eduardor0cha <c.eduardo_rn@outlook.com>
2022-02-12 21:37:15 -0300

    Fix(Login): Add Logout Button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
+import useAuth from '../../contexts/AuthProvider/useAuth';
+import Button from '../Button';
+
 function Header() {
   const [currentPage, setCurrentPage] = useState(String);
   const location = useLocation();
+
+  const { logout } = useAuth();
 
   useEffect(() => {
     switch (location.pathname) {
@@ -48,6 +53,7 @@ function Header() {
   return (
     <div className="sgpm-c-header">
       <h1>{currentPage}</h1>
+      <Button color="blue" text="Sair" onClick={() => logout()} />
     </div>
   );
 }
